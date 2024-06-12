634dd4aa6632c7262b179aaee58bac2f56380d1d
Joshua <insaddingaround@gmail.com>
2024-06-12 12:55:58 -0700

    star wars

---
+++ src/App.tsx
@@ -0,0 +1,203 @@
+import { useEffect, useState } from "react"
+import { DataTable } from "./components/DataTable"
+import "./App.css"
+
+
+const SW_API = "https://swapi.dev/api/"
+
+enum EntityType {
+  Starships = "starships",
+  People = "people",
+}
+
+type StarShip = {
+  name: string,
+  model: string,
+  manufacturer: string,
+  passengers: string,
+}
+
+type Film = {
+  title: string,
+  [EntityType.Starships]: string[] // array of urls
+  [EntityType.People]: string[] // array of urls
+}
+
+type State = {
+  films: Film[],
+  ids: string[],
+  entityType: EntityType,
+  details: StarShip[],
+  isLoading: boolean
+}
+
+function createColDefs(entity: EntityType) {
+  switch (entity) {
+    case EntityType.Starships:
+      return [
+        {
+          accessorKey: "name",
+          header: "Name",
+        },
+        {
+          accessorKey: "model",
+          header: "Model",
+        },
+        {
+          accessorKey: "manufacturer",
+          header: "Manufacturer",
+        },
+        {
+          accessorKey: "passengers",
+          header: "Passengers",
+        },
+      ]
+    default:
+      return []
+  }
+}
+
+function App() {
+  const [state, setState] = useState<State>({
+    films: [],
+    ids: [],
+    entityType: EntityType.Starships,
+    details: [],
+    isLoading: false
+  })
+
+  useEffect(function fetchFilms() {
+    // Set loading state
+    setState(prev => ({
+      ...prev,
+      isLoading: true
+    }))
+
+    // Fetch films
+    fetch(SW_API + "films").then(response => {
+      if (!response.ok) {
+        console.warn("error")
+      }
+      return response.json()
+    })
+      // Set films
+      .then((data: {
+        results: Film[]
+      }) => {
+        setState(prev => ({
+          ...prev,
+          films: data.results,
+        }))
+      })
+      // Catch errors
+      .catch(e => {
+        console.warn("error", e.message)
+      })
+      // Finally, set loading state to false
+      .finally(() => setState(prev => ({
+        ...prev,
+        isLoading: false,
+      })))
+  }, [])
+
+  useEffect(function fetchDetails() {
+    // If there are no ids, return
+    if (!state.ids.length) {
+      return;
+    }
+
+    // Set loading state
+    setState(prev => ({
+      ...prev,
+      isLoading: true
+    }))
+
+    // Fetch details
+    Promise.all(state.ids.map(id => {
+      return fetch(`${SW_API}/${state.entityType}/${id}`)
+    }))
+      .then(response => {
+        return Promise.all(response.map(res => res.json()))
+      })
+      // Set details
+      .then(data => {
+        setState(prev => ({
+          ...prev,
+          details: data
+        }))
+      })
+      // Set loading state to false
+      .finally(() => {
+        setState(prev => ({
+          ...prev,
+          isLoading: false
+        }))
+      })
+  }, [state.entityType, state.ids])
+
+  function handleFilmChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    const film = state.films.find(f => f.title === e.currentTarget.value)
+    if (!film || !state.entityType) {
+      return;
+    }
+    console.log("film", film)
+
+    const ids = film[state.entityType].map(url => url.split("/")[5]) ?? []
+    setState(prev => ({
+      ...prev,
+      ids
+    }))
+  }
+
+  function handleEntityChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    setState(prev => ({
+      ...prev,
+      entityType: e.target.value as EntityType,
+    }))
+  }
+
+  return (
+    <main className="text-left">
+      <header className="flex gap-2">
+        <select
+          defaultValue={state.films[0]?.title}
+          className="select w-full max-w-xs"
+          onChange={handleFilmChange}>
+          <option selected disabled>
+            Choose a film
+          </option>
+          {
+            state.films.map(film => {
+              return (
+                <option
+                  key={film.title}>{film.title}</option>
+              )
+            })
+          }
+        </select>
+
+        <select
+          className="select w-full max-w-xs"
+          value={state.entityType || undefined}
+          onChange={handleEntityChange}>
+          <option selected disabled>
+            Choose an entity
+          </option>
+          <option
+            value={EntityType.Starships}>
+            {EntityType.Starships}
+          </option>
+          <option
+            value={EntityType.People}>
+            {EntityType.People}
+          </option>
+        </select>
+
+      </header>
+      {state.isLoading && <div className="w-full p-4 flex gap-4">Loading... <span className=" loading loading-ring loading-xs"></span></div>}
+      <DataTable columns={createColDefs(EntityType.Starships)} data={state.details} />
+    </main>
+  )
+}
+
+export default App
